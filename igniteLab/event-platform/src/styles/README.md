d4389bbc20c5337f4519e7ed56274f8bc6ad70c6
bruholanda <brunaholandacunha7@gmail.com>
2022-06-23 12:24:38 -0300

    Comments and notes

---
+++ igniteLab/event-platform/src/styles/README.md
@@ -0,0 +1,4 @@
+# Notes
+
+### Tailwind:
+##### Biblioteca de CSS para melhorar a produtividade na estilização da Aplicação.
--- igniteLab/event-platform/tailwind.config.js
+++ igniteLab/event-platform/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  //Todos os arquivos da pasta src de extensão .tsx serão estilização com o Tailwind
   content: [
     './src/**/*.tsx'
   ],
